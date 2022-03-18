03b407720445bde4afd74ec12e369fe86a2362ce
EC2 Default User <ec2-user@ip-172-31-82-139.ec2.internal>
2022-03-18 13:58:34 +0000

    カラム変更前

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,19 +1,36 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
+
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to @product
+  end
+  
+  def index
+    @products = Product.all
+    
   end
 
   def show
+    @product = Product.find(params[:id])
+    
   end
 
-  def index
-  end
+ 
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:janre_id, :name, :about, :image, :price, :is_active)  
   end
 end
