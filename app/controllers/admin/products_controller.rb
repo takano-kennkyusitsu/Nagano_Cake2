3c3cbe03578295862dbca5839e1d8832c7ed1970
EC2 Default User <ec2-user@ip-172-31-82-139.ec2.internal>
2022-03-19 13:55:43 +0000

    商品画面

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,13 @@
 class Admin::ProductsController < ApplicationController
   def new
     @product = Product.new
-
+    @genres = Genre.all
   end
 
   def create
     @product = Product.new(product_params)
     if @product.save
-    redirect_to admin_products_path
+    redirect_to admin_product_path(@product)
     else
     render :new
     end
@@ -15,7 +15,6 @@ class Admin::ProductsController < ApplicationController
   
   def index
     @products = Product.page(params[:page])
- 
   end
 
   def show
@@ -25,22 +24,20 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    @genre = Genre.all
   end
 
   def update
     @product = Product.find(params[:id])
-    
     if @product.update(product_params)
-    redirect_to admin_product_path(@product) 
+    #redirect_to admin_products_path
     else
     render:edit
-  
     end
-    
   end
   
   private
   def product_params
-    params.require(:product).permit(:janre_id, :name, :about, :image, :price, :is_active)  
+    params.require(:product).permit(:genre_id, :name, :about, :image, :price, :is_active)  
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,14 +3,9 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
 
-  
-  
-  def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image
+ 
+  def get_product_image
+    product_image.attached? ? product_image : 'no_image.jpg'
   end
-
+  
 end
