c2ca5466a915e76ea9092385108f95a2c68214b8
marimo54 <98205353+marimo54@users.noreply.github.com>
2022-03-21 13:27:49 +0900

    Revert "Revert "Public/商品詳細画面""

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+ devise_for :admin, controllers: {
+  sessions: 'admin/sessions'
+ }
 
-
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+#devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  #sessions: "admin/sessions"
+#}
 namespace :admin do
   get 'orders/:order_id/order_details/:id' => 'order_details#update'
  end
- namespace :adimn do
+ namespace :adminn do
   resources :orders, only: [:show, :update]
  end
  namespace :admin do
@@ -25,24 +27,24 @@ namespace :admin do
   resources :products, only: [:new, :create, :show, :index, :edit, :update]
  end
  namespace :admin do
-  get '/' => 'homes#top'
+  get '/' => 'homes#top', as: 'top'
  end
+
+ scope module: :public do
  get '/' => 'homes#top'
  get '/about' => 'homes#about'
-  resources :shippings, only: [:create, :edit, :index, :update, :destroy]
-  resources :cart_items, only: [:index, :update, :destroy, :create]
+ resources :shippings, only: [:create, :edit, :index, :update, :destroy]
+ resources :cart_items, only: [:index, :update, :destroy, :create]
  get 'cart_items/destoroy_all'
-  resources :orders, only: [:new, :show, :index, :create]
+ resources :orders, only: [:new, :show, :index, :create]
  get 'orders/thank_you'
  get 'orders/confirm'
-  resources :products, only: [:index, :show]
-  resource :customers, only: [:edit, :update]
+ resources :products, only: [:index, :show]
+ resource :customers, only: [:edit, :update]
  get 'customers/my_page' => 'customers#show'
  get 'customers/unsubscribe'
  get 'customers/withdraw'
-
-
-
+ end
 
   #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
