6743d101e2c6c528c5afde12d5718113f584d32d
EC2 Default User <ec2-user@ip-172-31-85-81.ec2.internal>
2022-03-21 03:44:30 +0000

    commit_finish

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -72,7 +72,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
   			 keys: [:first_name, :last_name, :first_name_kana, :last_name_kana,
-                :email, :postcode, :address, :tel])
+                :email, :postcode, :address, :tel, :password])
 
   end
 end
