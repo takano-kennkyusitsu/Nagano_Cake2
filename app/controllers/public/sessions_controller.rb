0b191e018eb5e3bfd618c9ac715b6a0e7f690930
EC2 Default User <ec2-user@ip-172-31-85-81.ec2.internal>
2022-03-24 07:38:13 +0000

    commit_start

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -27,48 +27,16 @@ class Public::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  # protected
-=======
-
-  # protected
-=======
->>>>>>> 03c4a5119d69dab528751c8590392776c12b7d77
 
   # protected
 
-<<<<<<< HEAD
-=======
->>>>>>> origin/develop
->>>>>>> 00d783d25cd843de8d141a4cbe24ef6a8e9dc14f
-=======
->>>>>>> b7fbf2b904022885b35bf4faacc6d2fc26ce8724
-  # protected
->>>>>>> 02ea8e19fc7b6a9c537eff53ddc7da7e93fcf8f2
-=======
->>>>>>> 03c4a5119d69dab528751c8590392776c12b7d77
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-end
 
 
 
-
-
-
-
-=======
->>>>>>> 03c4a5119d69dab528751c8590392776c12b7d77
 
  def customer_state
  ## 【処理内容1】 入力されたemailからアカウントを1件取得
@@ -85,27 +53,4 @@ end
 end
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-end
->>>>>>> a9f127f2230d12d40d4d8912e4cb5c86dabd6cce
-=======
->>>>>>> 00d783d25cd843de8d141a4cbe24ef6a8e9dc14f
-=======
-end
->>>>>>> b7fbf2b904022885b35bf4faacc6d2fc26ce8724
-=======
-end
->>>>>>> 03c4a5119d69dab528751c8590392776c12b7d77
-=======
->>>>>>> 1581d0956ccc24fce3b6472af3f1f7c0fcdbea98
-=======
-
-
-
->>>>>>> 3972690a8d6f562ec5de8b0a4304c1b334022e37
